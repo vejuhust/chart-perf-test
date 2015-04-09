22017ada10e00d8ed1a1db38b856146b968fafaa
Wei Ye <vejuhust@gmail.com>
2015-04-09 20:10:50 +0800

    Add column & row control for the page

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-console.log("enter scripts!");
+$(document).ready(function() {
+    var _defaultColumn = 5;
+    var _defaultRow = 10;
 
-window.onload = function() {
-    alert("let's go!");
-}
+    console.log("script ready to run!");
+
+    var refreshButton = $("#refreshButton");
+    refreshButton.on("click", function() {
+        main();
+    });
+
+    main();
+
+
+
+    function getValueOrDefault(_id, _default) {
+        _id = "#" + _id;
+        _val = $(_id).val();
+        if (!_val) {
+            $(_id).val(_default);
+            _val = _default;
+        }
+        return _val;
+    }
+
+    function main () {
+        console.log("run main!");
+
+        column = getValueOrDefault("numberColumn", _defaultColumn)
+        row = getValueOrDefault("numberRow", _defaultRow)
+
+        var main = $("#main");
+        main.text("area for charts");
+        main.append("<div>wtf " + column + ", " + row + "</div>")
+    }
+
+});
